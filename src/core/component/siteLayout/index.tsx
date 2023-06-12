8adf2b2c783aee93d96b460aba028e5d85befd5b
tung le <tung.le5@onemount.com>
2023-06-12 14:46:12 +0700

    Add tooltip component

--- src/core/component/siteLayout/index.tsx
+++ src/core/component/siteLayout/index.tsx
@@ -4,28 +4,7 @@ import { actions } from 'core/redux/slice';
 import { RootState } from 'core/redux/reducer';
 
 export const SiteLayout: React.FC = ({ children }) => {
-    const { isOnline } = useSelector((state: RootState) => state.appReducer) || {};
-
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        // Update network status
-        const handleStatusChange = () => {
-            dispatch(actions.onHandleNetworkChange(window?.navigator?.onLine));
-        };
-
-        // Listen to the online status
-        window?.addEventListener('online', handleStatusChange);
-
-        // Listen to the offline status
-        window?.addEventListener('offline', handleStatusChange);
-
-        // Specify how to clean up after this effect for performance improvment
-        return () => {
-            window?.removeEventListener('online', handleStatusChange);
-            window?.removeEventListener('offline', handleStatusChange);
-        };
-    }, [isOnline]);
 
     return (
         <div>
--- src/core/redux/slice.ts
+++ src/core/redux/slice.ts
@@ -1,26 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AddressModel, UserModel } from 'model';
-import { TicketSelectModel } from '../model/event-detail/ticketSelect';
 
 interface SliceState {
     // Global State
-    ticket?: TicketSelectModel[];
-    ticketSelected?: TicketSelectModel[];
     loading?: boolean;
-    isOnline?: boolean;
-    userInfo?: UserModel;
-    address?: AddressModel[];
-    selectedAddess?: AddressModel;
 }
 
 const initialState: SliceState = {
-    ticket: [],
     loading: true,
-    ticketSelected: [],
-    isOnline: true,
-    userInfo: null,
-    address: [],
-    selectedAddess: null,
     // Global State
 };
 
@@ -28,44 +14,6 @@ const appSlice = createSlice({
     name: 'appSlice',
     initialState,
     reducers: {
-        getTickets: (state, action: PayloadAction<TicketSelectModel[]>) => {
-            state.ticket = action.payload;
-            state.loading = false;
-        },
-        changeTicket: (state, action: PayloadAction<TicketSelectModel>) => {
-            const checkItem = state.ticket.length > 0 && state.ticket.findIndex(i => i.id === action.payload.id);
-            if (typeof checkItem === 'boolean' || checkItem < 0) {
-                const ticketNew = [...state.ticket, action.payload];
-                state.ticket = ticketNew;
-            } else {
-                const ticketNew = [...state.ticket];
-                ticketNew[checkItem] = action.payload;
-                state.ticket = ticketNew;
-            }
-        },
-        removeTicket: (state, action) => {
-            const checkItem = state.ticket.length > 0 && state.ticket.findIndex(i => i.id === action.payload);
-            state.ticket[checkItem].amount_selected = Number(0);
-        },
-        removeAllTicket: (state, action) => {
-            state.ticket = action.payload;
-        },
-        onChangeTicketSelected: (state, action: PayloadAction<TicketSelectModel[]>) => {
-            state.ticketSelected = action.payload;
-        },
-        onHandleNetworkChange: (state, action: PayloadAction<boolean>) => {
-            state.isOnline = action.payload;
-        },
-        getUserInfo: (state, action: PayloadAction<UserModel>) => {
-            state.userInfo = action.payload;
-        },
-        getAddess: (state, action: PayloadAction<AddressModel[]>) => {
-            state.address = action.payload;
-            state.selectedAddess = action.payload?.find(address => address?.is_default);
-        },
-        handleSelectedAddess: (state, action: PayloadAction<AddressModel>) => {
-            state.selectedAddess = action.payload;
-        },
         // Global Actions
     },
 });
